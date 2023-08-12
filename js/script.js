2b17b17ccb9318698ae81e9940d799cef088c966
NduvhoEdward <Nduvho.Ramashia@gmail.com>
2023-08-12 22:05:13 +0200

    Add gameboard event listeners

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Cells click events...
+let cells = document.querySelectorAll(".cell");
+
+cells.forEach(cell => {
+  cell.addEventListener('click', play);
+});
+
+function play() {
+}
+
+
+// console.log(cells);
